a169c7d670d1f04c44b52b56ba7525c75b54312a
stmaabou <mahdi.abouzid@stud.h-da.de>
2023-10-16 23:45:05 +0200

    initialized project, added home Page

--- js/home.js
+++ js/home.js
@@ -1,7 +1,9 @@
+/**
+ * case 1: querySelectorAll to apply effects on all product containers within the page
+ */
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
-
 productContainers.forEach((item, i) => {
     let containerDimenstions = item.getBoundingClientRect();
     let containerWidth = containerDimenstions.width;
@@ -17,3 +19,21 @@ productContainers.forEach((item, i) => {
     })
 
 })
+
+/**
+ * case 2: apply effects on a single product container
+ */
+/*const nxtBtn = document.getElementById('nxt-btn');
+const preBtn = document.getElementById('pre-btn');
+let productContainer = document.getElementById('product-container');
+
+let containerDimenstions = productContainer.getBoundingClientRect();
+let containerWidth = containerDimenstions.width;
+
+nxtBtn.addEventListener('click', () => {
+    productContainer.scrollLeft += containerWidth ;
+})
+
+preBtn.addEventListener('click', () => {
+    productContainer.scrollLeft -= containerWidth ;
+})*/
